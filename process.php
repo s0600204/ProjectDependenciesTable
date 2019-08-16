319a3fe72fb502383614a757a6788b995e5a09de
s0600204 <dev@s0600204>
2019-08-16 19:59:05 +0100

    Change to making multiple simultaneous cURL requests
    Previously we were making a request, handling the response, making the next request,
    handling the response to that, and so on.
    
    Now we make multiple requests at the same time, and handle each response upon arrival.
    
    This should have the benefit of speeding up load-times, particularly on pages that
    require many requests to Repology's API.

--- process.php
+++ process.php
@@ -18,13 +18,32 @@ function version_compare2($a, $b)
 	return version_compare(implode('.', $a), implode('.', $b));
 }
 
+function process_response($ch, $dependency_code, &$latestVersion = NULL)
+{
+	$response = curl_multi_getcontent($ch);
+	$response = json_decode($response, true);
+	$versionsByDistro = array();
 
-$g_VersionsByDistro = array();
-$g_LatestVersions = array();
+	foreach ($response as $package)
+	{
+		if (in_array($package['status'], array('rolling', 'legacy')))
+			continue;
 
-$g_ch = curl_init();
-curl_setopt($g_ch, CURLOPT_HEADER, False);
-curl_setopt($g_ch, CURLOPT_RETURNTRANSFER, True);
+		if ($package['status'] == 'newest')
+			$latestVersion = $package['version'];
+
+		$distro = $package['repo'];
+		if (!isset($versionsByDistro[$distro])
+			|| version_compare2($package['version'], $versionsByDistro[$distro]) == 1)
+		{
+			$versionsByDistro[$distro] = $package['version'];
+		}
+	}
+
+	return $versionsByDistro;
+}
+
+$g_mh = curl_multi_init();
 
 $g_DistroList = array();
 foreach ($c_Distros as $distro)
@@ -46,48 +65,70 @@ foreach ($c_Dependencies as $dependency)
 			$g_DependencyList[] = $code;
 }
 
+// Set up the individual cURL handlers
+$g_DependencyCurls = array();
 foreach ($g_DependencyList as $dependency_code)
 {
-	curl_setopt($g_ch, CURLOPT_URL, "https://repology.org/api/v1/project/" . $dependency_code);
-	$response = curl_exec($g_ch);
-
-	if (curl_errno($g_ch))
-	{
-		echo 'Curl error: ' . curl_error($g_ch) . "<br/>\n";
-		continue;
-	}
-
-	$http_response_code = curl_getinfo($g_ch, CURLINFO_RESPONSE_CODE);
-	if ($http_response_code != 200)
-	{
-		echo "Unexpected response code: " . $http_response_code . "<br/>\n";
-		continue;
-	}
-
-	$response = json_decode($response, true);
-	foreach ($response as $package)
-	{
-		if (in_array($package['status'], array('rolling', 'legacy')))
-			continue;
-
-		if ($package['status'] == 'newest' && !isset($g_LatestVersions[$dependency_code]))
-			$g_LatestVersions[$dependency_code] = $package['version'];
-
-		if (in_array($package['repo'], $g_DistroList))
-		{
-			$distro = $package['repo'];
-			if (!isset($g_VersionsByDistro[$distro]))
-				$g_VersionsByDistro[$distro] = array($dependency_code => $package['version']);
-			else if (!isset($g_VersionsByDistro[$distro][$dependency_code])
-				|| (version_compare2($g_VersionsByDistro[$distro][$dependency_code], $package['version']) == -1 && $package['status'] != 'ignored')
-				|| $package['status'] == 'newest')
-			{
-				$g_VersionsByDistro[$distro][$dependency_code] = $package['version'];
-			}
-		}
-	}
+	$ch = curl_init();
+	curl_setopt_array($ch, array(
+		CURLOPT_HEADER => False,
+		CURLOPT_RETURNTRANSFER => True,
+		CURLOPT_URL => "https://repology.org/api/v1/project/" . $dependency_code
+	));
+	curl_multi_add_handle($g_mh, $ch);
+	$g_DependencyCurls[$dependency_code] = $ch;
 }
 
-curl_close($g_ch);
+$g_VersionsByDistro = array();
+$g_LatestVersions = array();
+
+do {
+	// Run the connection
+	$status = curl_multi_exec($g_mh, $num_handles_active);
+
+
+	// Block until activity
+	curl_multi_select($g_mh);
+
+	// Check for and deal with a completed response
+	do {
+		$info = curl_multi_info_read($g_mh, $msgs_in_queue);
+		if ($info)
+		{
+			echo ". ";
+			$ch = $info['handle'];
+
+			$http_response_code = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
+			if ($http_response_code == 200)
+			{
+				$dependency_code = basename(curl_getinfo($ch, CURLINFO_EFFECTIVE_URL));
+				$versions = process_response($ch, $dependency_code, $latestVersion);
+				foreach ($versions as $distro => $version)
+					if (in_array($distro, $g_DistroList))
+					{
+						if (!isset($g_VersionsByDistro[$distro]))
+							$g_VersionsByDistro[$distro] = array();
+						$g_VersionsByDistro[$distro][$dependency_code] = $version;
+					}
+				$g_LatestVersions[$dependency_code] = $latestVersion;
+			}
+			else
+				echo "Unexpected response code: " . $http_response_code . "<br/>\n";
+
+			// Remove this handle
+			curl_multi_remove_handle($g_mh, $ch);
+			$g_DependencyCurls[$dependency_code] = NULL;
+		}
+	} while ($msgs_in_queue > 0);
+
+} while ($num_handles_active && $status == CURLM_OK);
+
+// Remove any remaining handlers
+foreach ($g_DependencyCurls as $code => $ch)
+	if ($ch)
+		curl_multi_remove_handle($g_mh, $ch);
+unset($g_DependencyCurls);
+
+curl_multi_close($g_mh);
 
 ?>
