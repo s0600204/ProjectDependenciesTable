e8eae02c768fab749fa393878516090d36afbe31
s0600204 <dev@s0600204>
2019-08-24 18:08:43 +0100

    Allow the selection of different projects
    If a project is not specified, or not recognised, then a random project is shown.
    
    If a section of a project's dependencies is not specified, then the first section is shown.

--- data/projects.php
+++ data/projects.php
@@ -1,5 +1,21 @@
 <?php
 
+$selected_project_code = NULL;
+$selected_project_section = NULL;
+
+if (isset($_GET['project'])
+	&& mb_detect_encoding($_GET['project'], 'ASCII', true)
+) {
+	$selected_project_code = $_GET['project'];
+}
+
+if (isset($_GET['section'])
+	&& mb_detect_encoding($_GET['section'], 'ASCII', true)
+) {
+	$selected_project_section = $_GET['section'];
+}
+
+$c_Dependencies = array();
 $c_ProjectsPath = './data/projects/';
 $c_Projects = array();
 foreach (scandir($c_ProjectsPath, SCANDIR_SORT_ASCENDING) as $filename)
@@ -28,7 +44,19 @@ foreach (scandir($c_ProjectsPath, SCANDIR_SORT_ASCENDING) as $filename)
 		'url' => $project['url'],
 		'sections' => $dep_sections
 	);
-
 }
+
+if (!$selected_project_code || !in_array($selected_project_code, array_keys($c_Projects)))
+	$selected_project_code = array_keys($c_Projects)[rand(0, count($c_Projects) - 1)];
+
+$project = load_json($c_ProjectsPath . $selected_project_code . '.json');
+if (in_array($selected_project_section, $c_Projects[$selected_project_code]['sections']))
+	$c_Dependencies = $project['dependencies'][$selected_project_section];
+else
+	$c_Dependencies = $project['dependencies'][$c_Projects[$selected_project_code]['sections'][0]];
+
+unset($project);
+unset($selected_project_code);
+unset($selected_project_section);
 
 ?>
